b6324773fa035595d71e11ee541a319ef98abc7d
valentina-sierrat <123768493+valentina-sierrat@users.noreply.github.com>
2023-03-12 17:46:55 -0500

    bueno

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-document.addEventListener("DOMContentLoaded"),()=> {});
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "1",
+      img: "imagenes/ 1.jpeg"
+    },
+    {
+      name: "2",
+      img: "imagenes/ 2.jpg"
+    },
+    {
+      name: "3",
+      img: "imagenes/ 3.jpg"
+    },
+    {
+      name: "4",
+      img: "imagenes/ 4.jpg"
+    },
+    {
+      name: "5",
+      img: "imagenes/ 5.jpg"
+    },
+    {
+      name: "6",
+      img: "imagenes/ 6.jpg"
+    },
+    {
+      name: "7",
+      img: "imagenes/ 7.jpg"
+    },
+    {
+      name: "8",
+      img: "imagenes/ 8.jpg"
+    }
+  ];
 
+  const cuadricula = document.querySelector(".cuadricula");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
 
-const cardAdj = [{name: "perro", img: "images/c.png"}, {name: "cpp", img: "images/cpp.png"}, 
-{name: "css", img: "images/css.png"}, {name: "fortran", img: "images/fortran.png"},
-{name: "html", img: "images/html.png"}, {name: "python", img: "images/python.png"},
-{name: "c", img: "images/c.png"}, {name: "csharp", img: "images/csharp.png"}, 
-{name: "css", img: "images/css.png"}, {name: "fortran", img: "images/fortran.png"},
-{name: "html", img: "images/html.png"}, {name: "python", img: "images/python.png"}];
+  function creartablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasEscogidas = [];
-var cartasEscogidasId = [];
-var cartasGanadas = [];
+      carta.setAttribute("src", "images/reverso.png");
 
-function creartablero()¨{
-  for(let i=0; i< cardAdj.length; i++) ´{
-    var carta=document.createElement("img");
+      carta.setattribute("data-id", i);
 
+      carta.addEventListener("click", voltearcarta);
 
- carta.setAttribute("src","images/reverso.png");
- carta.setAttribute("data-id", i);
- carta.addEventListener("click",voltearCarta);
- cuadricula.appendChild(carta);
- 
-}
-
+      cuadricula.appendchild(carta);
+    }
+  }
+  function voltearcarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  creartablero();
+});
